eebc03b17b00e8734f530511f3874f9a34502e41
Clay Carpenter <claycarpenter@gmail.com>
2015-04-19 00:24:05 +0000

    Imports test of Node Jade module. Includes test Jade template file and module package manifest.

---
+++ jade/node-jade/index.js
@@ -0,0 +1,22 @@
+
+var jade = require('jade'),
+    Q = require('q'),
+    fs = require('fs'),
+    jadeFile = process.argv[2];
+
+if (!jadeFile) {
+    throw new Error('Missing command line argument to specify Jade template file.');
+}
+
+var fs_readFile = Q.denodeify(fs.readFile);
+
+fs_readFile(jadeFile, 'utf8')
+    .then(function (contents) {
+        var templateFn = jade.compile(contents, {pretty: true});
+        var html = templateFn({});
+        
+        console.log(html);
+    })
+    .catch(function (error) {
+        console.error(error.toString());
+    });
