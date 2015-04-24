79eb758bd771656c67da5538f9a1d3153f7482d2
Clay Carpenter <claycarpenter@gmail.com>
2015-04-24 00:39:39 +0000

    Initial import. Single passing test that parses arguments.

---
+++ testing/mocha-chai/file-finder/lib/tags.js
@@ -0,0 +1,31 @@
+
+exports = module.exports = Object.create(null);
+
+exports.parse = function (args) {
+    var options = Object.create(null);
+    
+    // Iterate over arguments.
+    args.forEach(function (argument) {
+        // Check for a long-formed tag (tag begins with double dashes: '--').
+        if (/^--/.test(argument)) {
+            argument = argument.substr(2);
+            
+            // Check for equals sign
+            if (argument.indexOf('=') !== -1) {
+                var argumentKeyValuePair = argument.split('=');
+                
+                var key = argumentKeyValuePair.shift(),
+                    value = argumentKeyValuePair.shift();
+                
+                if (/^[0-9]+$/.test(value)) {
+                    // Convert value from string into number.
+                    value = parseInt(value);
+                }
+                
+                options[key] = value;
+            }
+        }
+    })
+    
+    return options;    
+};
