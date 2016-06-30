74ea35d8b7a2b1c017592ee425c90030ddbb361a
Clay Carpenter <claycarpenter@gmail.com>
2016-06-29 21:37:41 -0400

    Adds example of investigating Jasmine spy calls.

--- karma-jasmine-example/test/pub-sub.spec.js
+++ karma-jasmine-example/test/pub-sub.spec.js
@@ -24,7 +24,33 @@ describe('PubSub', () => {
       expect(this.listener.callback).toHaveBeenCalled();
     });
 
-    it(`listener doesn't receivesnotification for channel event when not subscribed`, function() {
+    it('listener receives multiple notifications for subscribed channel event', function() {
+      // Arrange
+      spyOn(this.listener, 'callback');
+      this.pubSub.register(this.channelName, this.listener.callback);
+
+      // Act
+      this.pubSub.publish(this.channelName, 'first');
+      this.pubSub.publish(this.channelName, 'second');
+
+      // Assert
+
+      // Verify any calls were made
+      expect(this.listener.callback.calls.any()).toEqual(true);
+
+      // Verify exactly two calls were made
+      expect(this.listener.callback.calls.count()).toEqual(2);
+
+      // Verify first call payload
+      var firstCall = this.listener.callback.calls.first();
+      expect(firstCall.args[0]).toEqual('first');
+
+      // Verify last call payload
+      var lastCall = this.listener.callback.calls.mostRecent();
+      expect(lastCall.args[0]).toEqual('second');
+    });
+
+    it(`listener doesn't receive notification for channel event when not subscribed`, function() {
       // Arrange
       spyOn(this.listener, 'callback');
 
